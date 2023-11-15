cd23b517c945cbd4f8de98d8e7c502136434ddda
Namkhaidorj Batbaatar <23LP1872@G304-CC0082.local>
2023-11-15 19:03:25 +0800

    dom day 1

---
+++ dom.js
@@ -0,0 +1,17 @@
+const btn = document.getElementById("addBtn");
+ let i = 0;
+ function addBox() {
+    i++;
+    const box = document.createElement("div");
+    
+    box.classList.add("box")
+    box.innerText = i;
+
+    document.body.appendChild(box);
+ }
+ btn.addEventListener("click" , addBox)
+ //task 2
+  
+ const calculator = document.createElement("div");
+ calculator.classList.add(call)
+ 
